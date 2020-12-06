d0e149da84206d861b92ba2d5a0426e11cada35d
mcoronado <matias.coronado@usach.cl>
2020-12-06 00:58:36 -0300

    listooo 100%

--- lab2Parte1.m
+++ lab2Parte1.m
@@ -1,39 +1,41 @@
-%PARTE 1
+%Se definen las funciones H1 y H2
 H1num = [8 0];
 H1den = [6 2];
 H1 = tf(H1num,H1den);
-
 H2num = [5 7 4];
 H2den = [1 6 3];
 H2 = tf(H2num,H2den);
 
-%Abierto para H1
+%Grafico para el caso: Lazo abierto para H1
 figure(1)
 graficar(H1,'Lazo Abierto H1');
+%Datos de:  Lazo abierto para H1
 [H1_ceros H1_polos H1_ganancia] = tf2zp(H1num,H1den)
 H1_tmpEstab = stepinfo(H1).SettlingTime()
 
-%Abierto para H2
+%Grafico para el caso: Lazo abierto para H2
 figure(2)
 graficar(H2,'Lazo Abierto H2');
+%Datos de:  Lazo abierto para H2
 [H2_ceros H2_polos H2_ganancia] = tf2zp(H2num,H2den)
 H2_tmpEstab = stepinfo(H2).SettlingTime()
 
-%Cerrado para H1
+%Grafico para el caso: Lazo cerrado para H1
 H1_feedback = feedback(H1,-1);
 [H1Fnum H1Fden] = tfdata(H1_feedback, 'v');
-
 figure(3)
 graficar(H1_feedback,'Lazo Cerrado H1');
+
+%Datos de:  Lazo cerrado para H1
 [H1F_ceros H1F_polos H1F_ganancia] = tf2zp(H1Fnum,H1Fden)
 H1F_tmpEstab = stepinfo(H1_feedback).SettlingTime()
 
-%Cerrado para H2
+%Grafico para el caso: Lazo cerrado para H2
 H2_feedback = feedback(H2,-1);
 [H2Fnum H2Fden] = tfdata(H2_feedback, 'v');
-
 figure(4)
 graficar(H2_feedback,'Lazo Cerrado H2');
+%Datos de:  Lazo cerrado para H2
 [H2F_ceros H2F_polos H2F_ganancia] = tf2zp(H2Fnum,H2Fden)
 H2F_tmpEstab = stepinfo(H2_feedback).SettlingTime()
 
--- lab2Parte2.m
+++ lab2Parte2.m
@@ -1,6 +1,5 @@
-%PARTE 2
 ver control
-
+%Se definen las funciones Hn
 H_uno = tf(1,1);
 H1 = tf([0 0 8 0],[0 0 4 6]);
 H2 = tf([0 0 0 6],[0 0 1 5]);
@@ -9,15 +8,22 @@ H4 = tf([0 0 0 6],[0 0 7 1]);
 H5 = tf([0 0 5 3],[5 1 0 4]);
 H6 = tf([0 0 5 1],[1 6 0 5]);
 
+%Para la conexion paralela entre H1 y H2
 H_A = parallel(H1,H2);
+%Para el feedback de H3
 H_B1 = feedback(H3,H_uno,+1);
+%Para la conexion en serie del feedback H3 y H4
 H_B2 = series(H_B1,H4);
+%Para la conexion en serie del feedback H3 y H5
 H_B3 = series(H_B1,H5);
+%Para la conexion paralela entre H_B2 y H_B3
 H_B4 = parallel(H_B2,H_B3);
-
+%Para la conexion en serie de H_B4 y H6
 H_B = series(H_B4,H6);
+%Para la conexion paralela de H_A y H_B
 H_final = parallel(H_A,H_B)
 
+%Para graficar la respuesta escalon final
 graficar(H_final,"Respuesta escalon")
 
 
